6d441c25cfac56d1676945731559aa48c9f75481
Noble Obioma <nobioma1@gmail.com>
2024-03-06 13:59:02 +0000

    setup web test

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -13,5 +14,10 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    globals: true,
   },
 });
